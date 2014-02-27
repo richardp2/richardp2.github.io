4e73bee4c2ca166b70e9432070459a7501f8924c
Richard Perry <richard@perrymail.me.uk>
2014-02-27 08:01:02 +0000

    Update deploy script so rsync excludes hidden files

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,5 @@ cd ~/workspace/master/
 git add -A
 git commit -aF ../source/changes/
 git push origin master
-rsync -azh --progress --delete . perryon1@sftp.perry-online.me.uk:~/public_html/bGbDmSuXlg2MKV5PrIpJ/jekyll
+rsync -azh --progress --delete --exclude ".*" . perryon1@sftp.perry-online.me.uk:~/public_html/bGbDmSuXlg2MKV5PrIpJ/jekyll
 cd ~/workspace/source/
